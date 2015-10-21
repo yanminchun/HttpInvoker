404e3e98de4585b92fedc693fd2f7780b23e992b
yanminchun <937105113@qq.com>
2015-10-21 14:29:52 +0800

    模拟http请求的工具

---
+++ HttpInvoker/HttpDownload.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Text;
+
+namespace HttpInvoker
+{
+    public class HttpRequest
+    {
+        public HttpWebRequest WebRequest { get; set; }
+
+        public HttpRequest(string url)
+        {
+            WebRequest = (HttpWebRequest)System.Net.WebRequest.Create(url);
+
+            if (WebRequest.RequestUri.AbsoluteUri.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+            {
+                ServicePointManager.DefaultConnectionLimit = 50;
+                ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, errors) => { return true; };
+            }
+        }
+
+        public void SetPostData(string data, Encoding encoding)
+        {
+            if (data == null)
+                data = string.Empty;
+            if (encoding == null)
+                encoding = Encoding.UTF8;
+
+            Stream requestStream = null;
+            try
+            {
+                byte[] buffer = encoding.GetBytes(data);
+                WebRequest.ContentLength = buffer.Length;
+                requestStream = WebRequest.GetRequestStream();
+                requestStream.Write(buffer, 0, buffer.Length);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (requestStream != null)
+                {
+                    requestStream.Close();
+                    requestStream = null;
+                }
+            }
+        }
+    }
+
+    public class HttpUnInitException : Exception
+    {
+        public static readonly string UnInit = "HttpRequest尚未初始化化";
+
+        public HttpUnInitException() :
+            this(UnInit)
+        {
+
+        }
+
+        public HttpUnInitException(string message)
+        {
+
+        }
+    }
+
+    public class HttpDownload
+    {
+        private HttpRequest HttpRequest { get; set; }
+
+        public Encoding ResponseEncoding { get; set; }
+
+        public HttpDownload(HttpRequest httpRequest)
+        {
+            HttpRequest = httpRequest;
+            ResponseEncoding = Encoding.UTF8;
+        }
+
+        public HttpDownload(HttpRequest httpRequest, Encoding responseEncoding)
+            : this(httpRequest)
+        {
+            ResponseEncoding = responseEncoding;
+        }
+
+        public HttpResult GetResponse()
+        {
+            HttpResult result = new HttpResult();
+
+            try
+            {
+                if (HttpRequest.WebRequest == null)
+                {
+                    throw new HttpUnInitException();
+                }
+
+                using (HttpWebResponse response = (HttpWebResponse)(HttpRequest.WebRequest.GetResponse()))
+                {
+                    using (Stream stream = GetResponseStream(response))
+                    {
+                        result.Headers = response.Headers;
+                        result.StatusCode = response.StatusCode;
+                        result.StatusDescription = response.StatusDescription;
+                        result.Cookies = response.Cookies;
+
+                        using (MemoryStream _stream = new MemoryStream())
+                        {
+                            stream.CopyTo(_stream);
+                            result.ResponseBuffer = _stream.ToArray();
+                            result.ResponseString = ResponseEncoding.GetString(result.ResponseBuffer);
+                        }
+
+                        result.Success = true;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                result.Exception = e;
+            }
+
+            return result;
+        }
+
+        private Stream GetResponseStream(HttpWebResponse response)
+        {
+            Stream stream;
+            if (response.ContentEncoding.ToLower().Contains("gzip"))
+            {
+                stream = new GZipStream(response.GetResponseStream(), CompressionMode.Decompress);
+            }
+            else if (response.ContentEncoding.ToLower().Contains("deflate"))
+            {
+                stream = new DeflateStream(response.GetResponseStream(), CompressionMode.Decompress);
+            }
+            else
+            {
+                stream = response.GetResponseStream();
+            }
+            return stream;
+        }
+    }
+
+    public class HttpResult
+    {
+        public bool Success { get; set; }
+
+        public string ResponseString { get; set; }
+
+        public byte[] ResponseBuffer { get; set; }
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        public WebHeaderCollection Headers { get; set; }
+
+        public string StatusDescription { get; set; }
+
+        public CookieCollection Cookies { get; set; }
+
+        public Exception Exception { get; set; }
+    }
+}
---
+++ HttpInvoker/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HttpInvoker
+{
+    public class Pair
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        protected List<Pair> Pairs;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Pairs = new List<Pair>();
+            grd.DataContext = Pairs;
+        }
+
+        private void DataGrid_InitializingNewItem(object sender, InitializingNewItemEventArgs e)
+        {
+            ((Pair)e.NewItem).Name = "";
+            ((Pair)e.NewItem).Value = "";
+        }
+
+        private void grd_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                grd.BeginEdit();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string url = FixUrl(txtUrl.Text);
+            if (string.IsNullOrEmpty(url))
+            {
+                MessageBox.Show("Url not allow empty!");
+                return;
+            }
+
+            rtbRequestHeader.Document.Blocks.Clear();
+            rtbRequestBody.Document.Blocks.Clear();
+            rtbResponseHeader.Document.Blocks.Clear();
+            rtbResponseBody.Document.Blocks.Clear();
+            pb.IsIndeterminate = true;
+
+            string method = ((ComboBoxItem)cmbType.SelectedItem).Content.ToString();
+            var items = grd.DataContext as List<Pair>;
+            ThreadPool.QueueUserWorkItem((x) =>
+            {
+                try
+                {
+                    Invoke(url, method, items);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                    window.Dispatcher.Invoke(new Action(() =>
+                    {
+                        pb.IsIndeterminate = false;
+                    }));
+                }
+            });
+        }
+
+        public void Invoke(string url, string method, List<Pair> items)
+        {
+            StringBuilder builder = new StringBuilder();
+            if (items != null)
+            {
+                foreach (var item in items)
+                {
+                    builder.AppendFormat("{0}={1}&", item.Name, item.Value);
+                }
+            }
+
+            HttpRequest request = null;
+            if ("post".Equals(method, StringComparison.OrdinalIgnoreCase))
+            {
+                request = new HttpRequest(url);
+                request.WebRequest.Method = "POST";
+                request.SetPostData(builder.ToString(), Encoding.UTF8);
+            }
+            else
+            {
+                request = new HttpRequest(url + "?" + builder.ToString());
+                request.WebRequest.Method = "GET";
+            }
+
+            HttpDownload down = new HttpDownload(request);
+            HttpResult result = down.GetResponse();
+
+            window.Dispatcher.Invoke(new Action(() =>
+            {
+                rtbRequestHeader.AppendText(ParseHeader(request.WebRequest.Headers));
+                rtbRequestBody.AppendText(builder.ToString());
+                rtbResponseHeader.AppendText(ParseHeader(result.Headers));
+                rtbResponseBody.AppendText(result.ResponseString);
+                pb.IsIndeterminate = false;
+            }));
+        }
+
+        public string ParseHeader(WebHeaderCollection headers)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            if (headers != null)
+            {
+                foreach (var item in headers.AllKeys)
+                {
+                    builder.AppendFormat("{0} : {1}{2}", item, headers.Get(item), Environment.NewLine);
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        public string FixUrl(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+                return string.Empty;
+
+            url = url.Trim();
+
+            if (url.StartsWith("http://", StringComparison.OrdinalIgnoreCase))
+                return url;
+
+            if (url.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+                return url;
+
+            url = "http://" + url;
+            return url;
+        }
+    }
+}
